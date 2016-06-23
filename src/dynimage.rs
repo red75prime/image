0309dc8f071f58fa6bdfb27e37e6aab0a11bc66d
red75prime <red75prim@gmail.com>
2016-06-23 16:18:39 +0500

    hdr_pr sync

--- src/dynimage.rs
+++ src/dynimage.rs
@@ -550,7 +550,6 @@ fn open_impl(path: &Path) -> ImageResult<DynamicImage> {
         "bmp" => image::ImageFormat::BMP,
         "ico" => image::ImageFormat::ICO,
         "hdr" => image::ImageFormat::HDR,
-        "pic" => image::ImageFormat::PIC,
         format => return Err(image::ImageError::UnsupportedError(format!(
             "Image format image/{:?} is not supported.",
             format
@@ -616,9 +615,7 @@ pub fn load<R: Read+Seek>(r: R, format: ImageFormat) -> ImageResult<DynamicImage
         #[cfg(feature = "ico")]
         image::ImageFormat::ICO => decoder_to_image(try!(ico::ICODecoder::new(r))),
         #[cfg(feature = "hdr")]
-        image::ImageFormat::HDR => decoder_to_image(try!(hdr::HDRAdapter::new(r))),
-        #[cfg(feature = "hdr")]
-        image::ImageFormat::PIC => decoder_to_image(try!(hdr::HDRAdapter::new_nonstrict(r))),
+        image::ImageFormat::HDR => decoder_to_image(try!(hdr::HDRAdapter::new(BufReader::new(r)))),
         _ => Err(image::ImageError::UnsupportedError(format!("A decoder for {:?} is not available.", format))),
     }
 }
